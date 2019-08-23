015d361a13a943152ee55d1d1dc181c08864762c
kaploniak <michalkaploniak@gmail.com>
2019-08-23 15:15:17 +0100

    added api request for all articles

--- src/api.js
+++ src/api.js
@@ -1,3 +1,9 @@
+import axios from "axios";
 const baseURL = "https://news-app-kaploniak.herokuapp.com/api";
 
-export const fetchAllArticles = () => {};
+export const getAllArticles = async () => {
+  const { data } = await axios.get(`${baseURL}/articles`, {
+    params: {}
+  });
+  return data;
+};

eba937a4621aeb705db0ebe4d14697f1d4c70848
kaploniak <michalkaploniak@gmail.com>
2019-08-23 15:15:44 +0100

    added article list

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
+import Loading from "../utils/Loading";
+import * as api from "../api";
 
 class ArticleList extends Component {
   state = {
-    articles: []
+    articles: [],
+    page: 1,
+    isLoading: true
   };
   render() {
-    return <div />;
+    const { articles, isLoading } = this.state;
+    if (isLoading) return <Loading text="Loading the articles" />;
+    return (
+      <ul className="articleList">
+        {articles.map(article => {
+          return <li key={article.article_id}>{article.title}</li>;
+        })}
+      </ul>
+    );
   }
+
+  componentDidMount() {
+    this.fetchAllArticles();
+  }
+
+  fetchAllArticles = () => {
+    api.getAllArticles().then(({ articles }) => {
+      this.setState({ articles, isLoading: false });
+    });
+  };
 }
 
 export default ArticleList;

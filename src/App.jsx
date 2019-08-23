d1b98b5e8fa53f96f9c648afdb8b9dbe71095e34
kaploniak <michalkaploniak@gmail.com>
2019-08-23 15:13:44 +0100

    changed name of App.js to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Router } from "@reach/router";
+
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Homepage from "./components/Homepage";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Header />
+        <Router>
+          <Homepage path="/" />
+        </Router>
+        <Footer />
       </div>
     );
   }
 }
 
 export default App;
+
+// /api/articles

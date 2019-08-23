9a45ed965784fec27a607bd0721823329e0206e1
kaploniak <michalkaploniak@gmail.com>
2019-08-23 14:31:01 +0100

    added article list template componet

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class ArticleList extends Component {
+  state = {
+    articles: []
+  };
+  render() {
+    return <div />;
+  }
+}
+
+export default ArticleList;

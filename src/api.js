c4fc5f16b560bf69be9282739c3282d17617116d
kaploniak <michalkaploniak@gmail.com>
2019-08-27 09:29:11 +0100

    added get all comments by article id request

--- src/api.js
+++ src/api.js
@@ -1,9 +1,45 @@
 import axios from "axios";
 const baseURL = "https://news-app-kaploniak.herokuapp.com/api";
 
-export const getAllArticles = async () => {
+export const getAllArticles = async ({
+  sort_by,
+  order,
+  author,
+  topic,
+  limit,
+  page
+}) => {
   const { data } = await axios.get(`${baseURL}/articles`, {
+    params: {
+      sort_by,
+      order,
+      author,
+      topic,
+      limit,
+      page
+    }
+  });
+  console.log(data);
+  return data;
+};
+
+export const getArticleById = async article_id => {
+  const { data } = await axios.get(`${baseURL}/articles/${article_id}`, {
     params: {}
   });
+  return data;
+};
+
+export const getAllCommentsByArticleId = async ({
+  article_id,
+  sort_by,
+  order
+}) => {
+  const { data } = await axios.get(
+    `${baseURL}/articles/${article_id}/comments`,
+    {
+      params: { sort_by, order }
+    }
+  );
   return data;
 };

a903bb64260456bdeeb2b61462cda84c9a4e555a
kaploniak <michalkaploniak@gmail.com>
2019-08-27 09:33:50 +0100

    added header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <h1 className="header">NEWS APP HEADER</h1>;
+  return (
+    <h1 className="header">
+      <strong>NEWS APP HEADER</strong>
+    </h1>
+  );
 };
 
 export default Header;

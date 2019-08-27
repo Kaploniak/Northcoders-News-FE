2322669ca1b91591b56edd1e9e388116b6ea4730
kaploniak <michalkaploniak@gmail.com>
2019-08-27 09:29:35 +0100

    added loading spinner

--- src/utils/Loading.jsx
+++ src/utils/Loading.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Spinner from "react-bootstrap/Spinner";
 
 const Loading = ({ text }) => {
   return (
     <>
       <div className="loading">
+        <Spinner animation="border" variant="danger" />
         <h2>{text || "Loading..."}</h2>
       </div>
     </>

ca9eaaeead5a46ba421ee9d4cf2a8a3c5b02c905
kaploniak <michalkaploniak@gmail.com>
2019-08-27 09:35:52 +0100

    added article card component

---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import Loading from "../utils/Loading";
+import * as api from "../api";
+import Card from "react-bootstrap/Card";
+import { Link } from "@reach/router";
+import { datePrettier } from "../utils/utils";
+
+import cooking from "../images/cooking.jpg";
+import coding from "../images/coding.jpg";
+import football from "../images/football.jpg";
+
+class ArticleCard extends Component {
+  state = {
+    article: {},
+    isLoading: true
+  };
+  render() {
+    const { article, isLoading } = this.state;
+    const ref = {
+      coding,
+      cooking,
+      football
+    };
+    if (isLoading) return <Loading text="Loading the article..." />;
+    return (
+      <Card style={{ width: "18rem" }} className="card">
+        <Card.Body>
+          <Link to={`/articles?topic=${article.topic}`}>
+            <Card.Img
+              variant="top"
+              src={ref[article.topic]}
+              alt={`Card image: ${article.topic} topic`}
+            />
+          </Link>
+          <Card.Title>{article.title}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            {datePrettier(article.created_at)}
+          </Card.Subtitle>
+          <Link to={`/articles?author=${article.author}`}>
+            <Card.Subtitle className="mb-2 text-muted">
+              {article.author}
+            </Card.Subtitle>
+          </Link>
+          <Link to={`/articles?topic=${article.topic}`}>
+            <Card.Subtitle className="mb-2 text-muted">
+              #{article.topic}
+            </Card.Subtitle>
+          </Link>
+          <Card.Text>
+            {article.body.length > 100
+              ? article.body.slice(0, 100) + " ..."
+              : article.body}
+          </Card.Text>
+          <Card.Link href={`/articles/${article.article_id}`}>
+            Read more
+          </Card.Link>
+        </Card.Body>
+      </Card>
+    );
+  }
+
+  componentDidMount() {
+    this.fetchArticleById();
+  }
+
+  fetchArticleById = () => {
+    const {
+      article: { article_id }
+    } = this.props;
+    api.getArticleById(article_id).then(({ article }) => {
+      this.setState({ article, isLoading: false, type: article.type });
+    });
+  };
+}
+
+export default ArticleCard;

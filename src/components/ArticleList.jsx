cbb729dd3171d1c5437c8511e3a53c0382648b18
kaploniak <michalkaploniak@gmail.com>
2019-08-28 08:51:15 +0100

    added pagination,  and different query options

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,42 +2,100 @@ import React, { Component } from "react";
 import Loading from "../utils/Loading";
 import * as api from "../api";
 import ArticleCard from "./ArticleCard";
+import ArticlePagination from "./ArticlePagination";
+import ArticleSort from "./ArticleSort";
 
 class ArticleList extends Component {
   state = {
     articles: [],
-    page: 1,
+    p: 1,
     total_count: 0,
-    isLoading: true
+    isLoading: true,
+    pagination: null,
+    sort: null,
+    sort_by: null,
+    topic: null,
+    author: null,
+    order: null
   };
   render() {
-    const { articles, isLoading } = this.state;
+    const {
+      articles,
+      isLoading,
+      pagination,
+      total_count,
+      p,
+      sort
+    } = this.state;
     if (isLoading) return <Loading text="Loading the articles" />;
     return (
-      <ul className="articleList">
-        {articles.map(article => {
-          return (
-            <li key={article.article_id}>
-              <ArticleCard article={article} />
-            </li>
-          );
-        })}
-      </ul>
+      <>
+        {pagination && (
+          <ArticlePagination
+            total_count={total_count}
+            p={p}
+            updatePage={this.updatePage}
+          />
+        )}
+        {sort && (
+          <ArticleSort className="articleSort" handleClick={this.handleClick} />
+        )}
+        <ul className="articleList">
+          {articles.map(article => {
+            return (
+              <li key={article.article_id}>
+                <ArticleCard article={article} />
+              </li>
+            );
+          })}
+        </ul>
+        {pagination && (
+          <ArticlePagination
+            total_count={total_count}
+            p={p}
+            updatePage={this.updatePage}
+          />
+        )}
+      </>
     );
   }
 
+  handleClick = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   componentDidMount() {
+    const { pagination, sort, topic, author } = this.props;
     this.fetchAllArticles();
+    this.setState({ pagination, sort, topic, author });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { p, sort_by, order, topic, author } = this.state;
+
+    if (
+      prevState.p !== p ||
+      prevState.sort_by !== sort_by ||
+      prevState.order !== order ||
+      prevState.author !== author ||
+      prevState.topic !== topic
+    ) {
+      this.fetchAllArticles();
+    }
   }
 
   fetchAllArticles = () => {
-    const { sort_by, order, author, topic, limit, page } = this.props;
+    const { limit, topic, author } = this.props;
+    const { p, sort_by, order } = this.state;
     api
-      .getAllArticles({ sort_by, order, author, topic, limit, page })
+      .getAllArticles({ sort_by, order, limit, p, topic, author })
       .then(({ articles, total_count }) => {
-        console.log(total_count);
         this.setState({ articles, total_count, isLoading: false });
       });
+  };
+
+  updatePage = p => {
+    this.setState({ p });
   };
 }
 

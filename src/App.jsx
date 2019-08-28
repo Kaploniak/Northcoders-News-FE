e2ca1769fd7a0aa90773176f7efc040515b6f07d
kaploniak <michalkaploniak@gmail.com>
2019-08-28 08:50:00 +0100

    added state of username, imported new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { Router } from "@reach/router";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./components/Navigation";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
 import Footer from "./components/Footer";
-import Topics from "./pages/Topics";
+import AllTopicsPage from "./pages/AllTopicsPage";
 import ArticlePage from "./pages/ArticlePage";
 import AllArticlesPage from "./pages/AllArticlesPage";
 
 class App extends React.Component {
+  state = {
+    user: "jessjelly"
+    // user: null
+  };
   render() {
+    const { user } = this.state;
     return (
       <div className="App">
-        <Navigation />
+        <Navigation user={user} />
         <Header />
         <Router>
           <Homepage path="/" />
+          <AllArticlesPage path="/articles/:query" />
           <AllArticlesPage path="/articles" />
           <ArticlePage path="/articles/:article_id" />
-          <Topics path="/topics" />
+          <AllTopicsPage path="/topics" />
+          <Homepage default />
         </Router>
         <Footer />
       </div>

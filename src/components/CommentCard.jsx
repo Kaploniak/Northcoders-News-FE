3dd06942eb9da3f47430180b594473cef15400bf
kaploniak <michalkaploniak@gmail.com>
2019-08-29 12:50:55 +0100

    added delete comment button

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import Loading from "../utils/Loading";
 import { dateFormat, timeFormat } from "../utils/utils";
-import Card from "react-bootstrap/Card";
+import { Card, Button } from "react-bootstrap";
 import Voting from "./Voting";
+import * as api from "../api";
 // to do
 class CommentCard extends Component {
   state = {
@@ -12,10 +13,18 @@ class CommentCard extends Component {
   render() {
     const { comment, isLoading } = this.state;
     const { loggedInUser } = this.props;
+
     if (isLoading) return <Loading text="Loading comment..." />;
     return (
       <Card>
         <Card.Body>
+          {loggedInUser && loggedInUser === comment.author && (
+            <div className="articleButton">
+              <Button variant="danger" onClick={this.handleDeleteComment}>
+                Delete comment
+              </Button>
+            </div>
+          )}
           <Card.Title>{comment.author}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
             {dateFormat(comment.created_at)}
@@ -35,6 +44,14 @@ class CommentCard extends Component {
     const { comment } = this.props;
     this.setState({ comment, isLoading: false });
   }
+
+  handleDeleteComment = () => {
+    const { comment } = this.props;
+    api.deleteComment(comment.comment_id).then(() => {
+      this.props.fetchAllCommentsByArticleId();
+      console.log("succesfuly deleted comment");
+    });
+  };
 }
 
 export default CommentCard;

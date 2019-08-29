59acfdf767493e76a2f68c9f59281a893c8cc5a9
kaploniak <michalkaploniak@gmail.com>
2019-08-29 15:34:32 +0100

    refactored to functional component

--- src/components/ArticleSort.jsx
+++ src/components/ArticleSort.jsx
@@ -1,75 +1,68 @@
-import React, { Component } from "react";
+import React from "react";
 import { Dropdown, DropdownButton } from "react-bootstrap";
 
-class ArticleSort extends Component {
-  render() {
-    const { handleClick } = this.props;
-    return (
-      <DropdownButton
-        className="dropdownSort"
-        id="dropdown-item-button"
-        title="Sort"
-        variant="secondary"
+const ArticleSort = props => {
+  const { handleClick } = props;
+  return (
+    <DropdownButton
+      className="dropdownSort"
+      id="dropdown-item-button"
+      title="Sort"
+      variant="secondary"
+    >
+      <Dropdown.Item
+        as="button"
+        name="sort_by"
+        value="author"
+        onClick={handleClick}
       >
-        <Dropdown.Item
-          as="button"
-          name="sort_by"
-          value="author"
-          onClick={handleClick}
-        >
-          by author
-        </Dropdown.Item>
-        <Dropdown.Item
-          as="button"
-          name="sort_by"
-          value="votes"
-          onClick={handleClick}
-        >
-          by votes
-        </Dropdown.Item>
-        <Dropdown.Item
-          as="button"
-          name="sort_by"
-          value="topic"
-          onClick={handleClick}
-        >
-          by topic
-        </Dropdown.Item>
-        <Dropdown.Item
-          as="button"
-          name="sort_by"
-          value="comment_count"
-          onClick={handleClick}
-        >
-          by coments
-        </Dropdown.Item>
-        <Dropdown.Item
-          as="button"
-          name="sort_by"
-          value="created_at"
-          onClick={handleClick}
-        >
-          by create time
-        </Dropdown.Item>
-        <Dropdown.Item
-          as="button"
-          name="order"
-          value="asc"
-          onClick={handleClick}
-        >
-          ascending
-        </Dropdown.Item>
-        <Dropdown.Item
-          as="button"
-          name="order"
-          value="desc"
-          onClick={handleClick}
-        >
-          descending
-        </Dropdown.Item>
-      </DropdownButton>
-    );
-  }
-}
+        by author
+      </Dropdown.Item>
+      <Dropdown.Item
+        as="button"
+        name="sort_by"
+        value="votes"
+        onClick={handleClick}
+      >
+        by votes
+      </Dropdown.Item>
+      <Dropdown.Item
+        as="button"
+        name="sort_by"
+        value="topic"
+        onClick={handleClick}
+      >
+        by topic
+      </Dropdown.Item>
+      <Dropdown.Item
+        as="button"
+        name="sort_by"
+        value="comment_count"
+        onClick={handleClick}
+      >
+        by coments
+      </Dropdown.Item>
+      <Dropdown.Item
+        as="button"
+        name="sort_by"
+        value="created_at"
+        onClick={handleClick}
+      >
+        by create time
+      </Dropdown.Item>
+      <Dropdown.Item as="button" name="order" value="asc" onClick={handleClick}>
+        ascending
+      </Dropdown.Item>
+      <Dropdown.Item
+        as="button"
+        name="order"
+        value="desc"
+        onClick={handleClick}
+      >
+        descending
+      </Dropdown.Item>
+    </DropdownButton>
+  );
+};
 
 export default ArticleSort;

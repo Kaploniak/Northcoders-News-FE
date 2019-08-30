863594381d448084c4b887db517f9ef8fe408abb
kaploniak <michalkaploniak@gmail.com>
2019-08-30 11:14:05 +0100

    added sign in page

---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import { Form, Col, Card, Button } from "react-bootstrap";
+import * as api from "../api";
+import ReactDOM from "react-dom";
+import { navigate } from "@reach/router";
+
+class SignInPage extends Component {
+  state = {
+    username: "",
+    name: "",
+    avatar_url:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+  };
+  render() {
+    return (
+      <Card>
+        <Card.Body>
+          <Form
+            id="myForm"
+            className="formAddArticle"
+            ref={form => (this.messageForm = form)}
+            onSubmit={this.onSubmit}
+          >
+            <Form.Group controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                placeholder="Username"
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="name">
+              <Form.Label>Full Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Full Name"
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Row>
+              <Form.Group as={Col} controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" disabled />
+              </Form.Group>
+              <Form.Group as={Col} controlId="confirmPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  disabled
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Group controlId="avatar_url" onChange={this.handleChange}>
+              <Form.Label>Avatar</Form.Label>
+              <Form.Control placeholder="Avatar URL" />
+            </Form.Group>
+
+            <Button
+              variant="secondary"
+              type="submit"
+              onClick={this.handleSubmit}
+            >
+              Sign In
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    );
+  }
+  handleChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { username, name, avatar_url } = this.state;
+
+    api
+      .postNewUser({ username, name, avatar_url })
+      .then(() => {
+        ReactDOM.findDOMNode(this.messageForm).reset();
+        this.setState({
+          username: "",
+          name: "",
+          avatar_url:
+            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+        });
+        navigate(`/login`);
+      })
+      .catch(err => {
+        this.setState({ err, isLoading: false });
+      });
+  };
+}
+
+export default SignInPage;
+
+// {
+//   "username": "tickle122",
+//     "avatar_url": "https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg",
+//       "name": "Tom Tickle"
+// },
+
+// (/(?!^\d+$)^.+$/gm.test(username))

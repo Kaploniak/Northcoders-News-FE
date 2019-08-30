e1e579212718b9e63d01325ad2a912ef9b14e361
kaploniak <michalkaploniak@gmail.com>
2019-08-30 11:13:54 +0100

    added log out page

---
+++ src/pages/LogOutPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LogOutPage = () => {
+  return (
+    <>
+      <h1>You have succesfully logged out</h1>
+      <h1>See you soon!</h1>
+    </>
+  );
+};
+
+export default LogOutPage;

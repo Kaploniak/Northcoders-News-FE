66f0545b6a16b3171ed518f27424fd72a6f1ecb5
kaploniak <michalkaploniak@gmail.com>
2019-08-30 12:29:23 +0100

    changed name of the file

---
+++ src/components/TopicAdder.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Form, Card, Button } from "react-bootstrap";
+import * as api from "../api";
+import ErrorPage from "../pages/ErrorPage";
+
+class AddTopic extends Component {
+  state = {
+    slug: "",
+    description: "",
+    err: false
+  };
+  render() {
+    const { err } = this.state;
+    if (err) return <ErrorPage err={err} />;
+    return (
+      <div className="addTopicForm">
+        <Card>
+          <Card.Body>
+            <Form
+              id="myForm"
+              className="form"
+              ref={form => (this.messageForm = form)}
+              onSubmit={this.onSubmit}
+            >
+              <Form.Group controlId="slug">
+                <Form.Label>Topic name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder='e.g. "cars"'
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="description">
+                <Form.Label>Topic description</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder='e.g. "Everything about cars."'
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Button
+                variant="secondary"
+                type="submit"
+                onClick={this.handleSubmit}
+              >
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { slug, description } = this.state;
+    const { fetchAllTopics } = this.props;
+    api
+      .postNewTopic({ slug, description })
+      .then(() => {
+        ReactDOM.findDOMNode(this.messageForm).reset();
+        this.setState({ slug: "", description: "" });
+        fetchAllTopics();
+      })
+      .catch(err => {
+        this.setState({ err });
+      });
+  };
+}
+
+export default AddTopic;
